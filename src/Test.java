3402ac9c111c851991241bda7f8bf3f1adcda05b
freyafuglo <frey3246@stud.kea.dk>
2023-03-15 09:12:54 +0100

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Scanner input = new Scanner(new File("input.txt"));
+
+            while (input.hasNextLine())
+            {
+                System.out.println(input.nextLine());
+            }
+
+        } catch (FileNotFoundException e)
+        {
+            //throw new RuntimeException(e);
+            System.out.println("Error reading file: " + e);
+        }
+    }
+}
